ec3834cf5d526b628fa43a99c29c79c0f19361f5
Damian <naimadswdn@gmail.com>
2020-11-11 19:56:44 +0100

    Implement another parser

--- app.py
+++ app.py
@@ -5,51 +5,50 @@ import argparse
 import shelve
 
 
-def parse_arguments():
-    usage = """
-Simple app to search for a books under Polish National library.
-The only required argument is an author.
-        
-You can add more arguments, with following format: 
-    app.py <author> --kind e-book --publicationYear 2010
+def parse_arguments_search():
+    usage_search = """
+    Simple app to search for a books under Polish National library.
+    The only required argument is an author.
 
-Full list of arguments which can be used: 
-https://data.bn.org.pl/docs/bibs 
+    You can add more arguments, with following format: 
+        app.py search <author> --kind e-book --publicationYear 2010
 
-Examples:
-    app.py Brandon,Sanders <- will print list of Brandon Sanderson's books. 
-    app.py Sienkiewicz,Henryk --title Krzyżacy
-    app.py Herbert,Frank --title Diuna --publicationYear 1992
-    app.py Trudi,Canavan --kind e-book --title Nowicjuszka
-    
-Format of printed result:
-title: <title> 
-author: <author> 
-gentre: <gentre> 
-publicationYear: <publicationYear> 
-isbnIssn: <isbnIssn>
-id: <id>
+    Full list of arguments which can be used: 
+    https://data.bn.org.pl/docs/bibs 
 
-Example: 
+    Examples:
+        app.py search Brandon,Sanders <- will print list of Brandon Sanderson's books. 
+        app.py search  Sienkiewicz,Henryk --title Krzyżacy
+        app.py search  Herbert,Frank --title Diuna --publicationYear 1992
+        app.py search  Trudi,Canavan --kind e-book --title Nowicjuszka
+        app.py search Läckberg <- it is possible to use only a surname of author
 
-title: Krzyżacy /
-author: Sienkiewicz, Henryk (1846-1916)
-genre: Powieść polska
-publicationYear: 1995
-isbnIssn: 8370490239 8370490468
-id: 1006077
-        """
-    parser = argparse.ArgumentParser(usage=usage)
+    Format of printed result:
+    title: <title> 
+    author: <author> 
+    gentre: <gentre> 
+    publicationYear: <publicationYear> 
+    isbnIssn: <isbnIssn>
+    id: <id>
+
+    Example: 
+
+    title: Krzyżacy /
+    author: Sienkiewicz, Henryk (1846-1916)
+    genre: Powieść polska
+    publicationYear: 1995
+    isbnIssn: 8370490239 8370490468
+    id: 1006077
+            """
+    parser = argparse.ArgumentParser(usage=usage_search)
     parser.add_argument('author', type=str, help="In following format: Brandon,Sanders <- only coma separated.")
 
     parsed, unknown = parser.parse_known_args()
-
     for arg in unknown:
         if arg.startswith('--'):
             parser.add_argument(arg)
 
     args = parser.parse_args()
-
     return args
 
 
@@ -103,18 +102,42 @@ def show_library_content():
 
 
 def dispatcher():
-    if sys.argv[1] == 'search':
+    usage = """
+    app.py <action> ... 
+
+Possible actions:
+app.py search <author>               <- will search for books with specified author. 
+app.py show                          <- will show books in your local library file.
+app.py add <author> <id>             <- will add book identified by author and id to your local library file.
+    
+
+Example:
+    app.py search Sanderson,Brandon --title Elantris
+    app.py search Herbert,Frank --title Diuna --publicationYear 1992
+    app.py add Trudi,Canavan 5311484
+    app.py show 
+    
+For more information about app.py search usage, please run:
+    app.py search
+    """
+    parser = argparse.ArgumentParser(usage=usage)
+    parser.add_argument('action')
+    args, unknown = parser.parse_known_args()
+
+    if args.action == 'search':
         del sys.argv[1]
-        arguments = parse_arguments()
+        arguments = parse_arguments_search()
         books = lookup_for_books(**vars(arguments))
         show_results(books)
-    elif sys.argv[1] == 'add':
+    elif args.action == 'add':
         del sys.argv[1]
         author = sys.argv[1]
         book_id = sys.argv[2]
         add_to_shelve(author, book_id)
-    elif sys.argv[1] == 'show':
+    elif args.action == 'show':
         show_library_content()
+    else:
+        print('Wrong argument! Please run app.py --help for correct usage information. ')
 
 
 if __name__ == "__main__":
